2a086b748561979503c46be32c70a656fb949242
Pavel Yastremsky <goodieart@gmail.com>
2022-11-15 04:46:49 +0300

    Main() filled; GetUserInput() fixed; PrintArray() fixed

--- Program.cs
+++ Program.cs
@@ -2,35 +2,55 @@
 
 class Program
 {
+    const int MAX_LENGTH = 3;
     static void Main(string[] args)
     {
-        string[] inArray = GetUserInput();
-        PrintArray(inArray);
-        WriteLine($"Количество элементов длиной < 3: {CountItemsByLen(inArray)}"); 
+            string[] inArray = GetUserInput(prompt: "Введите строки через запятую для отбора по длине");
+            PrintArray(inArray, "Входной массив");
+
+            int outLength = CountItemsByLen(inArray, MAX_LENGTH);
+            string[] outArray = new string[outLength];
+
+            int i = 0;
+            foreach (string item in inArray)
+            {
+                if (item.Length <= MAX_LENGTH)
+                {
+                    outArray[i] = item;
+                    i++;
+                }
+            }
+            PrintArray(outArray, "Выходной массив");
     }
+    
     /// <summary>
     /// Метод возвращает ввод пользователя, преобразованный в строковый массив
     /// </summary>
     /// <param name = "separator">Строковый разделитель; символы между ними в исходной строке будут
     /// преобразованы в элементы результирующего массива</param>
+    /// <param name = "prompt">Приглашение для пользователя</param>
     /// <returns>Новый массив, заполненный преобразованными строковыми значениями в целочисленный тип</returns>
-    static string[] GetUserInput(string separator = ",")
+    static string[] GetUserInput(string separator = ",", string prompt = "")
     {
+        if (prompt != String.Empty) Write($"{prompt}: ");
         return Console.ReadLine()!.Split(separator, StringSplitOptions.RemoveEmptyEntries);
     }
+    
     /// <summary>
     /// Метод выводит на экран содержимое переданного одномерного массива с вещественными числами
     /// </summary>
     /// <param name = "inArray">Массив, содержимое которого следует вывести на экран</param>
-    static void PrintArray(string[] inArray)
+    /// <param name = "prompt">Приглашение для пользователя</param>
+    static void PrintArray(string[] inArray, string prompt = "")
     {
-        Write("[");
+        Write($"{prompt}{(prompt == String.Empty ? "" : ": ")}[");
         for (int i = 0; i < inArray.Length; i++)
         {
             Write($"{inArray[i]}{(i < inArray.Length - 1 ? "," : "")}");
         }
-        Write("]");
+        WriteLine("]");
     }
+    
     /// <summary>
     /// Метод возвращает количество элементов массива строкового типа с длиной меньше maxItemLength
     /// </summary>
