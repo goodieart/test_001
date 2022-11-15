1555ae9c3f6d2ed49890403598aac4373ec939d2
Pavel Yastremsky <goodieart@gmail.com>
2022-11-15 04:06:38 +0300

    CountItemsByLen() added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-
+        string[] inArray = GetUserInput();
+        PrintArray(inArray);
+        WriteLine($"Количество элементов длиной < 3: {CountItemsByLen(inArray)}"); 
     }
     /// <summary>
     /// Метод возвращает ввод пользователя, преобразованный в строковый массив
@@ -20,7 +22,7 @@ class Program
     /// Метод выводит на экран содержимое переданного одномерного массива с вещественными числами
     /// </summary>
     /// <param name = "inArray">Массив, содержимое которого следует вывести на экран</param>
-    public static void PrintArray(string[] inArray)
+    static void PrintArray(string[] inArray)
     {
         Write("[");
         for (int i = 0; i < inArray.Length; i++)
@@ -28,5 +30,23 @@ class Program
             Write($"{inArray[i]}{(i < inArray.Length - 1 ? "," : "")}");
         }
         Write("]");
+    }
+    /// <summary>
+    /// Метод возвращает количество элементов массива строкового типа с длиной меньше maxItemLength
+    /// </summary>
+    /// <param name = "inArray">Входной массив</param>
+    /// <param name = "maxItemLength">Максимальная длина элемента</param>
+    /// <returns>Количетсво элементов входного массива с длиной меньше maxItemLength</returns>
+    static int CountItemsByLen(string[] inArray, int maxItemLength = 3)
+    {
+        int result = 0;
+        foreach (string item in inArray)
+        {
+            if (item.Length <= maxItemLength)
+            {
+                result++;
+            }
+        }
+        return result;
     }
 }
