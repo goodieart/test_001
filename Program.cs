6c70ab0fbc8ccbb3fb03a1465d2bc1088d2809f3
Pavel Yastremsky <goodieart@gmail.com>
2022-11-15 03:40:32 +0300

    GetUserInput() added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    /// <summary>
+    /// Метод возвращает ввод пользователя, преобразованный в строковый массив
+    /// </summary>
+    /// <param name = "separator">Строковый разделитель; символы между ними в исходной строке будут
+    /// преобразованы в элементы результирующего массива</param>
+    /// <returns>Новый массив, заполненный преобразованными строковыми значениями в целочисленный тип</returns>
+    static string[] GetUserInput(string separator = ",")
+    {
+        return Console.ReadLine()!.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+    }
+}
